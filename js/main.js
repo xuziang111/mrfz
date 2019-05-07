2a11ec1d42922051286994376cb56d8856918c2e
xuziang111 <584788096@qq.com>
2019-05-07 20:52:50 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+(function(){
+var vm = new Vue({
+    el:'main',
+    data:{
+        item:{
+           needa1:0,
+           needa2:0,
+           needa3:0,
+           needb1:0,
+           needb2:0,
+           needb3:0,
+           needb4:0,
+           needb5:0,
+           needb6:0,
+           needb7:0,
+           needb8:0,
+           needb9:0,
+           needb10:0,
+           resultb1:0,
+           resultb2:0,
+           resultb3:0,
+           resultb4:0,
+           resultb5:0,
+           resultb6:0,
+           resultb7:0,
+           resultb8:0,
+           resultb9:0,
+           resultb10:0,
+        }
+    }
+})
+
+
+
+
+
+}())
